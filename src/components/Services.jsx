98429fe946902ce6a9073b17df9f233026280dfd
um500 <113470418+um500@users.noreply.github.com>
2024-08-02 13:41:38 +0530

    update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 
 const cardData = [
   {
-    id: "1",
+    id: "Carbon_Footprint_Calculator",
     image: new URL("../assets/service/carfoot.jpeg", import.meta.url).href,
     heading: "Carbon Footprint Calculator",
     description:
@@ -59,7 +59,7 @@ const cardData = [
     ],
   },
   {
-    id: "2",
+    id: "Sustainability_Hub",
     image: new URL("../assets/service/sustainabilityhub.jpeg", import.meta.url).href,
     heading: "Sustainability Hub",
     description: "Articles, videos, infographics, expert opinions, and blog on sustainability.",
@@ -112,7 +112,7 @@ const cardData = [
     ]
   },
   {
-    id: "3",
+    id: "Eco_Challenges",
     image: new URL("../assets/service/EcoChall.jpeg", import.meta.url).href,
     heading: "Eco-Challenges",
     description: "Interactive challenges with rewards and social media sharing.",
@@ -165,7 +165,7 @@ const cardData = [
     ]
   },  
   {
-    id: "4",
+    id: "Sustainable_Living_Guide",
     image: new URL("../assets/service/sustainableguid.jpg", import.meta.url).href,
     heading: "Sustainable Living Guide",
     description: "Tips, goal tracking, and app integration for sustainable habits.",
@@ -218,7 +218,7 @@ const cardData = [
     ]
   },  
   {
-    id: "5",
+    id: "Community_Forum",
     image: new URL("../assets/service/communityform.jpg", import.meta.url).href,
     heading: "Community Forum",
     description: "Discussion board, expert Q&As, webinars, and group management.",
@@ -276,7 +276,7 @@ const cardData = [
   },  
   
   {
-    id: "6",
+    id: "Business_Sustainability_Toolkit",
     image: new URL("../assets/service/bussiness.jpeg", import.meta.url).href,
     heading: "Business Sustainability Toolkit",
     description: "Resources, case studies, and guides for sustainable business practices.",
@@ -330,7 +330,7 @@ const cardData = [
   },
 
   {
-    id: "7",
+    id: "News_and_Updates",
     image: new URL("../assets/service/newsupdate.jpg", import.meta.url).href,
     heading: "News and Updates",
     description: "Latest environmental news, updates, and newsletter subscription.",
@@ -382,7 +382,7 @@ const cardData = [
     ]
   },
   {
-    id: "8",
+    id: "Tree_Plantation",
     image: new URL("../assets/service/tree.jpg", import.meta.url).href,
     heading: "Tree Plantation",
     description: "Tree planting is the process of transplanting tree seedlings to new locations, aimed at restoring and enhancing green spaces.",
